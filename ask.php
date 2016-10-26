6b1fde0d6fa3c71086d0db4fc88a16fa6b75b4dd
SumanKoley <sumaneceuit@gmail.com>
2016-10-27 02:25:36 +0530

    Create ask.php

---
+++ ask.php
@@ -0,0 +1,11 @@
+<?php
+$json_file = file_get_contents('sample.json');
+$stats = json_decode($json_file);
+$id=$_GET['id'];
+foreach ($stats->status as $i => $stat) {
+	if(substr($stat->id,0,3) == $id){
+		echo dechex(substr($stat->pin16,0,1)*8+substr($stat->pin05,0,1)*4+substr($stat->pin04,0,1)*2+substr($stat->pin00,0,1));
+	} 
+}
+?><?php
+?>
